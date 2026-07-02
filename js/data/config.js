b0eedea073750f7a606a46fcac8cdeea272b682a
marymaryranting-collab <marymaryranting@gmail.com>
2026-07-02 14:22:19 +0200

    Update config.js

--- js/data/config.js
+++ js/data/config.js
@@ -1,1 +1,55 @@
+/*
+==========================================
+Yuri After Story
+Version: 1.0.0
+Configuration
+==========================================
+*/
 
+"use strict";
+
+const CONFIG = Object.freeze({
+
+    // -------------------------
+    // Game Information
+    // -------------------------
+
+    TITLE: "Yuri After Story",
+
+    VERSION: "1.0.0",
+
+    AUTHOR: "Mary",
+
+    // -------------------------
+    // Save System
+    // -------------------------
+
+    SAVE_KEY: "yuri_after_story_save",
+
+    // -------------------------
+    // Dialogue
+    // -------------------------
+
+    DEFAULT_SPEAKER: "Yuri",
+
+    TYPING_SPEED: 30,
+
+    // -------------------------
+    // Affection
+    // -------------------------
+
+    STARTING_AFFECTION: 0,
+
+    MAX_AFFECTION: 9999,
+
+    // -------------------------
+    // UI
+    // -------------------------
+
+    DEFAULT_BACKGROUND:
+        "assets/background/library.png",
+
+    DEFAULT_SPRITE:
+        "assets/sprites/yuri.png"
+
+});
