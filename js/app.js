4b20712536c8d85379d44b9174a3970cbcda1b1a
marymaryranting-collab <marymaryranting@gmail.com>
2026-07-02 14:21:16 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,51 @@
+/*
+==========================================
+Yuri After Story
+Version: 1.0.0
+Main Application
+==========================================
+*/
 
+"use strict";
+
+// -----------------------------
+// Global Game State
+// -----------------------------
+
+let saveData = null;
+
+// -----------------------------
+// Start Game
+// -----------------------------
+
+function startGame() {
+
+    loadGame();
+
+    saveData.visits++;
+
+    saveGame();
+
+    refreshUI();
+
+    nextDialogue();
+
+}
+
+// -----------------------------
+// Continue Button
+// -----------------------------
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const nextButton = document.getElementById("nextButton");
+
+    nextButton.addEventListener("click", () => {
+
+        nextDialogue();
+
+    });
+
+    startGame();
+
+});
